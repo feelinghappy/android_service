16c8e340ded9f1945da2f378fcc2fab4a8494db0
fanxiaoli <312889426@qq.com>
2017-06-18 14:32:29 +0800

    直接进入拨号界面拨出指定号码

--- CallTest/app/src/main/java/fanxiaoli/calltest/MainActivity.java
+++ CallTest/app/src/main/java/fanxiaoli/calltest/MainActivity.java
@@ -1,10 +1,16 @@
 package fanxiaoli.calltest;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +22,8 @@ public class MainActivity extends AppCompatActivity {
         mbutton01.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_CALL_BUTTON);
+                Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:12345678"));
+
                 startActivity(intent);
             }
         });
